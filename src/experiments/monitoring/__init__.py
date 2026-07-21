007fc9a96f19b77ef68bbaed62484d93ae8c9bdf
duanitamir <tamir.duani@zalando.de>
2026-07-21 14:17:45 +0200

    refactor: remove legacy code (centralized artifacts)

--- src/experiments/monitoring/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Monitoring and progress tracking for GA experiments."""
-
-from src.experiments.monitoring.ga_progress_monitor import (
-    GAProgressMonitor,
-    ExperimentProgressTracker,
-)
-
-__all__ = ["GAProgressMonitor", "ExperimentProgressTracker"]
